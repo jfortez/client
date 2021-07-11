bcad517fd32edb3c97bc740a0022a9281f21b9cb
Jean Caiza <jeanmcm@hotmail.com>
2021-07-10 23:47:51 -0500

    empezar a agregar tabla

--- src/components/layouts/sidebar/Sidebar.js
+++ src/components/layouts/sidebar/Sidebar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Sidebar.css";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { Dashboard } from "@material-ui/icons";
 
 const Sidebar = ({ isActive }) => {
@@ -9,7 +9,7 @@ const Sidebar = ({ isActive }) => {
       <div className={`slidebar ${isActive ? "" : "slidebar-expand"}`}>
         <ul className="slidebar-nav">
           <li className="slidebar-nav-item">
-            <NavLink to="/dashboard" className="slidebar-nav-link">
+            <NavLink exact to="/dashboard" className="slidebar-nav-link">
               <div>
                 <Dashboard className="slide-icon" />
               </div>
@@ -17,15 +17,19 @@ const Sidebar = ({ isActive }) => {
             </NavLink>
           </li>
           <li className="slidebar-nav-item">
-            <Link to="/dashboard/test" className="slidebar-nav-link">
+            <NavLink exact to="/dashboard/test" className="slidebar-nav-link">
               <div>
                 <Dashboard className="slide-icon" />
               </div>
               <span>Test</span>
-            </Link>
+            </NavLink>
           </li>
           <li className="slidebar-nav-item">
-            <NavLink to="/dashboard/personal" className="slidebar-nav-link">
+            <NavLink
+              exact
+              to="/dashboard/personal"
+              className="slidebar-nav-link"
+            >
               <div>
                 <Dashboard className="slide-icon" />
               </div>
@@ -33,7 +37,11 @@ const Sidebar = ({ isActive }) => {
             </NavLink>
           </li>
           <li className="slidebar-nav-item">
-            <NavLink to="/dashboard/pacientes" className="slidebar-nav-link">
+            <NavLink
+              exact
+              to="/dashboard/pacientes"
+              className="slidebar-nav-link"
+            >
               <div>
                 <Dashboard className="slide-icon" />
               </div>
@@ -41,19 +49,15 @@ const Sidebar = ({ isActive }) => {
             </NavLink>
           </li>
           <li className="slidebar-nav-item">
-            <NavLink to="/dashboard/usuarios" className="slidebar-nav-link">
+            <NavLink
+              exact
+              to="/dashboard/usuarios"
+              className="slidebar-nav-link"
+            >
               <div>
                 <Dashboard className="slide-icon" />
               </div>
               <span>Usuarios</span>
-            </NavLink>
-          </li>
-          <li className="slidebar-nav-item">
-            <NavLink to="/dashboard/test" className="slidebar-nav-link">
-              <div>
-                <Dashboard className="slide-icon" />
-              </div>
-              <span>Test</span>
             </NavLink>
           </li>
         </ul>
--- src/components/views/PacientesView.js
+++ src/components/views/PacientesView.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Topbar from "../layouts/topbar/Topbar";
+import axios from "axios";
 
 const PacientesView = () => {
+  const [pacientes, setPacientes] = useState([]);
+  const getPacientes = () => {
+    axios
+      .get("http://192.168.0.100:5000/api/pacientes")
+      .then((response) => {
+        setPacientes(response.data);
+      })
+      .catch((error) => console.error(error));
+  };
+  useEffect(() => {
+    let unmounted = false;
+    if (!unmounted) {
+      getPacientes();
+    }
+    return () => {
+      console.log("clean up");
+      unmounted = true;
+    };
+  });
+
   return (
     <>
       <Topbar />
       <div className="wrapper">
         <h1>Pacientes</h1>
+        <div>
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>Pacientes</li>
+              <li>Home</li>
+            </ul>
+          </nav>
+        </div>
+        <div>Agregar</div>
+        <div>
+          <ul>
+            {pacientes.map((paciente) => (
+              <li key={paciente.id_Paciente}>{paciente.nombres}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
