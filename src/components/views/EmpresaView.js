cb2fa9615d427ce0a1dfad3cca84c3338a6ddd15
Jean <jeanmcm@hotmail.com>
2021-08-20 17:11:59 -0500

    empresa

--- src/components/views/EmpresaView.js
+++ src/components/views/EmpresaView.js
@@ -1,13 +1,9 @@
 import React, { useState, useEffect } from "react";
-import {
-  Formulario,
-  ContenedorBotonCentrado,
-  Boton,
-  MensajeError,
-} from "../../elements/Formularios";
-import { Error } from "@material-ui/icons";
+import { Formulario, ContenedorBotonCentrado, Boton } from "../../elements/Formularios";
 import ComponentInput from "../layouts/forms/ComponentInput";
 import services from "../../services/empresa";
+import expresiones from "../../utils/Expresiones";
+import notificacion from "../../utils/Notificaciones";
 const EmpresaView = () => {
   const [id, setId] = useState(0);
   const [ruc, setRuc] = useState({ campo: "", valido: null });
@@ -16,57 +12,40 @@ const EmpresaView = () => {
   const [direccion2, setDireccion2] = useState({ campo: "", valido: null });
   const [ciudad, setCiudad] = useState({ campo: "", valido: null });
   const [telefono, setTelefono] = useState({ campo: "", valido: null });
-  const [formValid, setFormValid] = useState(null);
   const [isListed, setIsListed] = useState(false);
-  const expresiones = {
-    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-    direccion: /^[a-zA-Z0-9À-ÿ\s]{4,100}$/, // Letras, numeros, Acentos
-    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-    telefono: /^\d{7,14}$/, // 7 a 14 numeros.
-    numero: /^\d{5,14}$/, // 7 a 14 numeros.
-  };
   const onSubmit = async (e) => {
     e.preventDefault();
-    if (
-      ruc.valido === "true" &&
-      nombre.valido === "true" &&
-      direccion.valido === "true" &&
-      ciudad.valido === "true"
-    ) {
-      const newEmpresa = {
-        ruc: ruc.campo,
-        nombre: nombre.campo,
-        direccion: direccion.campo,
-        direccion2: direccion2.campo,
-        ciudad: ciudad.campo,
-        telefono: telefono.campo,
-      };
-      await services.saveEmpresa(newEmpresa, id);
-      setFormValid(true);
-      setIsListed(true);
-    } else {
-      setFormValid(false);
-    }
+    const newEmpresa = {
+      ruc: ruc.campo,
+      nombre: nombre.campo,
+      direccion: direccion.campo,
+      direccion2: direccion2.campo,
+      ciudad: ciudad.campo,
+      telefono: telefono.campo,
+    };
+    await services.saveEmpresa(newEmpresa, id);
+    notificacion("Mensaje", "Se ha guardado los datos con exito", "success");
+    setIsListed(!isListed);
+    // notificacion("Error", "Debe rellenar los campos requeridos", "danger");
   };
   const getEmpresa = async () => {
     const empresa = await services.optionEmpresa();
     const { id, ruc, nombre, direccion, direccion2, ciudad, telefono } = empresa[0];
     setId(id);
-    setRuc({ campo: ruc, valido: "true" });
-    setNombre({ campo: nombre, valido: "true" });
-    setDireccion({ campo: direccion, valido: "true" });
-    setDireccion2({ campo: direccion2, valido: null });
-    setCiudad({ campo: ciudad, valido: "true" });
+    setRuc({ campo: ruc, valido: null });
+    setNombre({ campo: nombre, valido: null });
+    setDireccion({ campo: direccion, valido: null });
+    setDireccion2({ campo: direccion2 ? direccion2 : "", valido: null });
+    setCiudad({ campo: ciudad, valido: null });
     setTelefono({ campo: telefono, valido: null });
   };
   useEffect(() => {
     getEmpresa();
-    setIsListed(false);
   }, [isListed]);
   return (
     <>
       <div>
-        <h1>Información de la Empresa</h1>
+        <h2>Información de la Empresa</h2>
         <Formulario onSubmit={onSubmit}>
           <ComponentInput
             state={ruc} //value
@@ -76,7 +55,7 @@ const EmpresaView = () => {
             name="ruc"
             placeholder="RUC"
             error="El Campo es requerido"
-            expresion={expresiones.numero}
+            expresion={expresiones.ruc}
           />
           <ComponentInput
             state={nombre} //value
@@ -96,7 +75,6 @@ const EmpresaView = () => {
             name="telefono"
             placeholder="Telefono"
           />
-          <br />
           <ComponentInput
             state={direccion} //value
             setState={setDireccion} //onChange
@@ -123,17 +101,8 @@ const EmpresaView = () => {
             name="ciudad"
             placeholder="Ciudad"
             error="El Campo es requerido"
-            expresion={expresiones.direccion}
+            expresion={expresiones.nombre}
           />
-          {/* Validacion */}
-          {formValid === false && (
-            <MensajeError>
-              <p>
-                <Error />
-                <b>Error: </b> Por favor rellene el formulario correctamente
-              </p>
-            </MensajeError>
-          )}
           <ContenedorBotonCentrado>
             <Boton type="submit">Guardar</Boton>
           </ContenedorBotonCentrado>
