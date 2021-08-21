c4c68dac954a76ebdcff6a835c80aa6e744d60fc
Jean <jeanmcm@hotmail.com>
2021-08-20 19:37:28 -0500

    personalizacion panel config

--- src/components/views/ConfigView.js
+++ src/components/views/ConfigView.js
@@ -1,10 +1,11 @@
 import "./DashboardView.css";
 import Topbar from "../layouts/topbar/Topbar";
 import useValues from "../../provider/useValues";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import EmpresaView from "./EmpresaView";
 import AccountView from "./AccountView";
 import BackUpView from "./BackUpView";
+import { AccountBox, Business, Storage } from "@material-ui/icons";
 
 const ConfigView = (props) => {
   const { pathname } = props.location;
@@ -14,44 +15,61 @@ const ConfigView = (props) => {
     <>
       <Topbar />
       <div className={`wrapper ${isCollapsed ? "sidebar-collapsed" : ""}`}>
-        <h1>Configuraciones</h1>
-        <div>
+        <h3 className="titulo">Configuración</h3>
+        <div className="navegacion">
           <nav>
             <ul>
               <li>
-                <NavLink to="/dashboard">Home</NavLink>
+                <Link to="/dashboard" className="navegacion__redirect">
+                  Home
+                </Link>
               </li>
+              <li> / </li>
               <li>
-                <NavLink to="/dashboard/settings">Configuración</NavLink>
+                <b>Configuración</b>
               </li>
             </ul>
           </nav>
         </div>
-        <h3>Navegación</h3>
-        <div>
-          <ul>
-            <li>
-              <NavLink to="/dashboard/settings/account">Configuración de la Cuenta</NavLink>
-            </li>
-            <li>
-              <NavLink to="/dashboard/settings/empresa">Datos de Compañía</NavLink>
-            </li>
-            <li>
-              <NavLink to="/dashboard/settings/backup">Respaldo</NavLink>
-            </li>
-          </ul>
+        <div className="row__config">
+          <div className="col-left">
+            <div className="vertical-menu">
+              <NavLink to="/dashboard/settings/account" className="link">
+                <span className="navlink__icon">
+                  <AccountBox className="navlink__icon__icon" />
+                </span>
+                <span className="navlink__title">Cuenta</span>
+              </NavLink>
+              <NavLink to="/dashboard/settings/empresa" className="link">
+                <span className="navlink__icon">
+                  <Business className="navlink__icon__icon" />
+                </span>
+                <span className="navlink__title">Empresa</span>
+              </NavLink>
+              <NavLink to="/dashboard/settings/backup" className="link">
+                <span className="navlink__icon">
+                  <Storage className="navlink__icon__icon" />
+                </span>
+                <span className="navlink__title">Respaldo</span>
+              </NavLink>
+            </div>
+          </div>
+          <div className="col-right">
+            <div className="contenido">
+              {pathname === "/dashboard/settings" ? (
+                <h4>Configuración</h4>
+              ) : pathname === "/dashboard/settings/empresa" ? (
+                <EmpresaView />
+              ) : pathname === "/dashboard/settings/account" ? (
+                <AccountView />
+              ) : pathname === "/dashboard/settings/backup" ? (
+                <BackUpView />
+              ) : (
+                <h1>nada</h1>
+              )}{" "}
+            </div>
+          </div>
         </div>
-        {pathname === "/dashboard/settings" ? (
-          <h4>Configuración</h4>
-        ) : pathname === "/dashboard/settings/empresa" ? (
-          <EmpresaView />
-        ) : pathname === "/dashboard/settings/account" ? (
-          <AccountView />
-        ) : pathname === "/dashboard/settings/backup" ? (
-          <BackUpView />
-        ) : (
-          <h1>nada</h1>
-        )}
       </div>
     </>
   );
--- src/components/views/PageTest.js
+++ src/components/views/PageTest.js
@@ -1,103 +1,47 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Topbar from "../layouts/topbar/Topbar";
 import useValues from "../../provider/useValues";
-import jsPDFInvoiceTemplate from "jspdf-invoice-template";
-import empresaServices from "../../services/empresa";
+import { NavLink } from "react-router-dom";
+import { Accessible } from "@material-ui/icons";
+
 const PageTest = () => {
   const { isCollapsed } = useValues();
-  const [empresa, setEmpresa] = useState([]);
-  useEffect(() => {
-    const getEmpresa = async () => {
-      const compañia = await empresaServices.optionEmpresa();
-      setEmpresa(compañia);
-    };
-    getEmpresa();
-  }, []);
-  var props = {
-    outputType: "save",
-    returnJsPDFDocObject: true,
-    fileName: "Factura No456871",
-    orientationLandscape: false,
-    logo: {
-      src: "https://raw.githubusercontent.com/jfortez/vitasmile/main/logo.png",
-      width: 53.33, //aspect ratio = width/height
-      height: 26.66,
-      margin: {
-        top: 0, //negative or positive num, from the current position
-        left: 0, //negative or positive num, from the current position
-      },
-    },
-    business: {
-      name: empresa[0]?.nombre,
-      address: `RUC: ${empresa[0]?.ruc}`,
-      phone: `Dirección: ${empresa[0]?.direccion}`,
-      email: `Telefono: ${empresa[0]?.telefono}`,
-      email_1: "",
-      website: "",
-    },
-    contact: {
-      label: "Invoice issued for:",
-      name: "Client Name",
-      address: "Albania, Tirane, Astir",
-      phone: "(+355) 069 22 22 222",
-      email: "client@website.al",
-      otherInfo: "www.website.al",
-    },
-    invoice: {
-      label: "Invoice #: ",
-      num: 19,
-      invDate: "Payment Date: 01/01/2021 18:12",
-      invGenDate: "Invoice Date: 02/02/2021 10:17",
-      headerBorder: false,
-      tableBodyBorder: false,
-      header: ["#", "Title", "Description", "Price", "Quantity", "Unit", "Total"],
-      table: Array.from(Array(30), (item, index) => [
-        index + 1,
-        "There are many variations ",
-        "Lorem Ipsum is simply dummy text ",
-        200.5,
-        4.5,
-        "m2",
-        400.5,
-      ]),
-      invTotalLabel: "Total:",
-      invTotal: "145,250.50",
-      invCurrency: "ALL",
-      row1: {
-        col1: "VAT:",
-        col2: "20",
-        col3: "%",
-        style: {
-          fontSize: 10, //optional, default 12
-        },
-      },
-      row2: {
-        col1: "SubTotal:",
-        col2: "116,199.90",
-        col3: "ALL",
-        style: {
-          fontSize: 10, //optional, default 12
-        },
-      },
-      invDescLabel: "Invoice Note",
-      invDesc:
-        "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
-    },
-    footer: {
-      text: "The invoice is created on a computer and is valid without the signature and stamp.",
-    },
-    pageEnable: true,
-    pageLabel: "Page ",
-  };
-  const handleCreatePdf = () => {
-    jsPDFInvoiceTemplate(props);
-  };
+
   return (
     <>
       <Topbar />
+      ertical-menu
       <div className={`wrapper ${isCollapsed ? "sidebar-collapsed" : ""}`}>
-        <h1>Página de Prueba</h1>
-        <button onClick={handleCreatePdf}>Crear</button>
+        <h1>Página de Pertical-menurueba</h1>
+        <div className="row">
+          <div className="col-left">
+            <h1>Left</h1>
+            <div className="vertical-menu">
+              <NavLink to="/dashboard/test/link1" className="link">
+                <span className="navlink__icon">
+                  <Accessible />
+                </span>
+                <span className="navlink__title">Cuenta</span>
+              </NavLink>
+              <NavLink to="/dashboard/test/link2" className="link">
+                <span className="navlink__icon">
+                  <Accessible />
+                </span>
+                <span className="navlink__title">Empresa</span>
+              </NavLink>
+              <NavLink to="/dashboard/test/link3" className="link">
+                <span className="navlink__icon">
+                  <Accessible />
+                </span>
+                <span className="navlink__title">Respaldo</span>
+              </NavLink>
+            </div>
+          </div>
+          <div className="col-right">
+            <h1>Right</h1>
+            <h1>test</h1>
+          </div>
+        </div>
       </div>
     </>
   );
