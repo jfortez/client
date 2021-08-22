74472d98109a326928ec829dd83f302efa7307f0
Jean <jeanmcm@hotmail.com>
2021-08-22 06:26:21 -0500

    home

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -2,30 +2,36 @@ import React from "react";
 import Topbar from "../layouts/topbar/Topbar";
 import "../views/DashboardView.css";
 import useValues from "../../provider/useValues";
-// import home from "../../img/home.jpg";
+
 const Dashboard = () => {
   const { isCollapsed, user } = useValues();
+  var img = new Image();
+  img.src = "/img/logo.png";
   return (
     <>
       <Topbar />
       <div className={`wrapper ${isCollapsed ? "sidebar-collapsed" : ""}`}>
-        <div className="test__">
-          <div className="home__img"></div>
-        </div>
-        <div className="home__img"></div>
-
         <div className="row">
           <div className="col-12">
             <div className="home__page home__header">
-              <h2>Bienvenido {user?.usuario}</h2>
+              <div>
+                <h2 className="home__title">
+                  BIENVENIDO <span>{user?.usuario}</span>
+                </h2>
+              </div>
+              <img
+                src={`${process.env.PUBLIC_URL + "/img/logo.png"}`}
+                alt="logo"
+                className="home__img"
+              />
             </div>
           </div>
         </div>
-        {/* <div className="row">
+        <div className="row">
           <div className="col-12">
             <div className="home__page home__body"></div>
           </div>
-        </div> */}
+        </div>
       </div>
     </>
   );
